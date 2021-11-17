a84b88c698ea8b98884a53315a11eb828ca89900
bachbnt <bachbnt@gmail.com>
2021-11-17 23:28:32 +0700

    fix

--- lib/networks/auth.dart
+++ lib/networks/auth.dart
@@ -7,14 +7,14 @@ import 'package:flutter/material.dart';
 import 'package:heath_care/model/user.dart';
 import 'package:heath_care/repository/user_repository.dart';
 import 'package:heath_care/ui/home_screen.dart';
+import 'package:heath_care/ui/login_screen.dart';
 import 'package:heath_care/ui/main_screen.dart';
 import 'package:heath_care/utils/api.dart';
+import 'package:heath_care/utils/http_exception.dart';
 import 'package:heath_care/utils/navigation_util.dart';
 import 'package:http/http.dart' as http;
-import 'package:heath_care/utils/http_exception.dart';
 import 'package:intl/intl.dart';
 import 'package:oktoast/oktoast.dart';
-
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -47,6 +47,11 @@ class Auth with ChangeNotifier {
     return tokenTmp != null && tokenTmp.isNotEmpty;
   }
 
+  Future<String> get role async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('role') ?? "";
+  }
+
   Future<String?> get token async {
     if (_token == null) {
       _token = await getToken();
@@ -69,21 +74,31 @@ class Auth with ChangeNotifier {
   }
 
   Future<void> logout() async {
-    _token = null;
-    _userEmail = null;
-    _userId = null;
-    _expiryDate = null;
+    try {
+      await userRepository.updateUserOnline(0);
+      _token = null;
+      _userEmail = null;
+      _userId = null;
+      _expiryDate = null;
 
-    if (_authTimer != null) {
-      _authTimer.cancel();
-      _authTimer = null;
+      if (_authTimer != null) {
+        _authTimer.cancel();
+        _authTimer = null;
+      }
+
+      notifyListeners();
+
+      final pref = await SharedPreferences.getInstance();
+      pref.clear();
+      DioCacheManager(CacheConfig(baseUrl: Api.authUrl)).clearAll();
+      NavigationUtil.pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => LoginPage(),
+        ),
+      );
+    } catch (error) {
+      showToast('Đăng xuất lỗi');
     }
-
-    notifyListeners();
-
-    final pref = await SharedPreferences.getInstance();
-    pref.clear();
-    DioCacheManager(CacheConfig(baseUrl: Api.authUrl)).clearAll();
   }
 
   void _autologout() {
@@ -156,22 +171,8 @@ class Auth with ChangeNotifier {
       // final format = new DateFormat('ss');
       // print(exDate);
 
-      String role = responseData['data']['roles'][0];
-      if (role == 'admin') {
-        NavigationUtil.pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => MainScreen(),
-          ),
-        );
-      } else if (role == 'Quản lý') {
-        NavigationUtil.pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => HomeScreen(),
-          ),
-        );
-      } else {
-        showToast('Đăng nhập lỗi');
-      }
+      await userRepository.updateUserOnline(1);
+
       User user = await userRepository.getCurrentUser(tokenTmp: _token);
       setCurrentUser(user);
       _expiryDate = DateTime.now().add(Duration(seconds: 600));
@@ -189,8 +190,28 @@ class Auth with ChangeNotifier {
       prefs.setString('userData', userData);
       setToken(_token);
       setExpiryDate(_expiryDate.toString());
+
+      String role = responseData['data']['roles'][0];
+      prefs.setString('role', role);
+
+      if (role == 'admin') {
+        NavigationUtil.pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => MainScreen(),
+          ),
+        );
+      } else if (role == 'Quản lý') {
+        NavigationUtil.pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ),
+        );
+      } else {
+        showToast('Đăng nhập lỗi');
+      }
     } catch (e) {
       print(e.toString());
+      showToast('Đăng nhập lỗi');
       throw e;
     }
   }
--- lib/ui/user_profile_screen.dart
+++ lib/ui/user_profile_screen.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:heath_care/model/district.dart';
+import 'package:heath_care/model/province.dart';
 
 import 'components/NavSideBar.dart';
 
-class UserProfileScreen extends StatelessWidget{
+class UserProfileScreen extends StatefulWidget {
+  @override
+  _UserProfileScreenState createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  List<Province> provinces = [
+    Province(provinceId: 0, name: 'HCM', isActive: 1),
+    Province(provinceId: 1, name: 'HN', isActive: 1),
+    Province(provinceId: 2, name: 'Đồng Nai', isActive: 1),
+    Province(provinceId: 3, name: 'Bình Dương', isActive: 1),
+    Province(provinceId: 4, name: 'Long An', isActive: 0)
+  ];
+
+  Province? selectedProvince;
+  List<District> districts = [];
+  District? selectedDistrict;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,11 +29,71 @@ class UserProfileScreen extends StatelessWidget{
           backgroundColor: Color.fromRGBO(78, 159, 193, 1),
           title: Text("HỒ SƠ"),
         ),
-        body: Container(
-          child: Text("PROFILE Page"),
+        body: Column(
+          children: [
+            DropdownButton<Province>(
+              hint: Text(selectedProvince?.name ?? 'vui lòng chọn'),
+              items: provinces.map((Province province) {
+                return DropdownMenuItem<Province>(
+                  value: province,
+                  child: Text(province.name!),
+                );
+              }).toList(),
+              onChanged: (province) {
+                print(province!.provinceId);
+                //có provinceId call api
+                //lúc này ta có lis district
+                //rebuild lại page này, sẽ hiện dropdown district vì length lúc này >0
+                setState(() {
+                  selectedProvince = province;
+                  districts = [
+                    District(
+                        districtId: 0,
+                        name: 'quan 1',
+                        province:
+                            Province(provinceId: 0, name: 'HCM', isActive: 1),
+                        isActive: 1),
+                    District(
+                        districtId: 1,
+                        name: 'quan 2',
+                        province:
+                            Province(provinceId: 0, name: 'HCM', isActive: 1),
+                        isActive: 1),
+                    District(
+                        districtId: 2,
+                        name: 'quan 3',
+                        province:
+                            Province(provinceId: 0, name: 'HCM', isActive: 1),
+                        isActive: 1),
+                    District(
+                        districtId: 3,
+                        name: 'quan 4',
+                        province:
+                            Province(provinceId: 0, name: 'HCM', isActive: 1),
+                        isActive: 1),
+                  ];
+                });
+              },
+            ),
+            if (districts.length > 0)
+              DropdownButton<District>(
+                hint: Text(selectedDistrict?.name ?? 'vui lòng chọn'),
+                items: districts.map((District district) {
+                  return DropdownMenuItem<District>(
+                    value: district,
+                    child: Text(district.name!),
+                  );
+                }).toList(),
+                onChanged: (district) {
+                  print(district!.districtId);
+                  setState(() {
+                    selectedDistrict = district;
+                  });
+                  //tượng tự có districtId, làm tiếp
+                },
+              )
+          ],
         ),
         drawer: NavDrawer());
-
   }
-
 }
